cf04c65da28072262e50db1bc86014fdef467f47
zachcran <zachcran@gmail.com>
2022-11-14 11:32:01 -0600

    Add simple library

---
+++ include/cmake_public_depend/cmake_public_depend.hpp
@@ -0,0 +1,6 @@
+#ifndef __CMAKE_PUBLIC_DEPEND_HPP
+#define __CMAKE_PUBLIC_DEPEND_HPP
+
+#include <cmake_public.hpp>
+
+#endif // __CMAKE_PUBLIC_DEPEND_HPP
---
+++ src/cmake_public_depend/cmake_public_depend.cpp
@@ -0,0 +1,5 @@
+#include "cmake_public_depend/cmake_public_depend.hpp"
+
+int call_cmake_public_depend() {
+    return call_cmake_public() + 2;
+}
